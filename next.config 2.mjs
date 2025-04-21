954f78856afbe819c043c512c2ed5f602c673f3b
yusa <yusaaihara@gmail.com>
2025-04-21 20:21:11 +0900

    1

---
+++ next.config 2.mjs
@@ -0,0 +1,14 @@
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
