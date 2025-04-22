4e18f73ee0203a138877f464e0b7f7798caafe32
tsuchidarikuto <tsuchidarikuto@gmail.com>
2025-04-22 22:14:33 +0900

    feat: implement POST API route for generating recipe responses

---
+++ app/api/ai/query/route.ts
@@ -0,0 +1,11 @@
+export async function POST(request: Request) {
+    const body = await request.json();
+    console.log(body);
+    //openaiapiを叩き、質問への回答を生成
+    return Response.json({
+        response: "スパゲッティ・カルボナーラは、卵、チーズ、パンチェッタと黒胡椒を使った伝統的なイタリアンパスタ料理です。";
+        timer?:{
+            minutes:"10:00"
+        }
+    })
+}
